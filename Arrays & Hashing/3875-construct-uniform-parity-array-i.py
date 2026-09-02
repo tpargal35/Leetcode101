9fd5f903d458b3e65c01c7e0abfb7ae2024bdb12
tpargal35 <tpargal35@gmail.com>
2026-09-02 20:05:42 +0530

    Solve 3875. Construct Uniform Parity Array I

---
+++ Arrays & Hashing/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,12 @@
+from typing import List, Optional, Dict
+
+def uniformArray(nums1: List[int]) -> bool:
+    return True
+
+# Example 1
+nums1 = [2, 3]
+print(uniformArray(nums1))  # Expected: True
+
+# Example 2
+nums2 = [4, 6]
+print(uniformArray(nums2))  # Expected: True
